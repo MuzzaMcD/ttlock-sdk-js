c8eb14333d28f1b656824a67ccfcdccb415c5c59
MuzzaMcD <72003592+MuzzaMcD@users.noreply.github.com>
2024-10-19 19:56:29 +1100

    Update getPassageMode.js

--- examples/getPassageMode.js
+++ examples/getPassageMode.js
@@ -12,14 +12,12 @@ async function doStuff() {
   client.startScanLock();
   console.log("Scan started");
   client.on("foundLock", async (lock) => {
-  //  console.log(lock.toJSON());
     console.log();
     
     if (lock.isInitialized() && lock.isPaired()) {
       await lock.connect();
-      console.log("Trying to set passage mode");
       console.log();
-      console.log();
+      console.log("Trying to GET passage mode");
       const result = await lock.getPassageMode();
       console.log(result);
       await lock.disconnect();
