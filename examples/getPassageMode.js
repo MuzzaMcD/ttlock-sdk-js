2cdd6573e6fae7d8e22a9731a3c18e8256e7ee2b
MuzzaMcD <72003592+MuzzaMcD@users.noreply.github.com>
2024-10-20 21:22:33 +1100

    Update getPassageMode.js

--- examples/getPassageMode.js
+++ examples/getPassageMode.js
@@ -3,6 +3,8 @@
 const { TTLockClient, sleep, PassageModeType } = require('../dist');
 const settingsFile = "lockData.json";
 
+require('log-timestamp')(function() { return new Date().toLocaleTimeString()+" | " });
+
 async function doStuff() {
   let lockData = await require("./common/loadData")(settingsFile);
   let options = require("./common/options")(lockData);
@@ -13,18 +15,19 @@ async function doStuff() {
   console.log("Scan started");
   client.on("foundLock", async (lock) => {
     console.log();
-    
+
     if (lock.isInitialized() && lock.isPaired()) {
       await lock.connect();
       console.log();
       console.log("Trying to GET passage mode");
       const result = await lock.getPassageMode();
-      console.log(result);
-      await lock.disconnect();
 
+      await lock.disconnect();
+      console.log("###");
       process.exit(0);
     }
   });
 }
 
 doStuff();
+
