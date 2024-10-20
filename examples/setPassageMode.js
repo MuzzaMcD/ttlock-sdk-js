3fc108e3e79d858192c41bc3c75acab3e45c124a
MuzzaMcD <72003592+MuzzaMcD@users.noreply.github.com>
2024-10-20 21:46:24 +1100

    Update setPassageMode.js

--- examples/setPassageMode.js
+++ examples/setPassageMode.js
@@ -2,6 +2,7 @@
 
 const { TTLockClient, sleep, PassageModeType } = require('../dist');
 const settingsFile = "lockData.json";
+require('log-timestamp')(function() { return new Date().toLocaleTimeString()+" | " });
 
 async function doStuff() {
   let lockData = await require("./common/loadData")(settingsFile);
@@ -12,14 +13,12 @@ async function doStuff() {
   client.startScanLock();
   console.log("Scan started");
   client.on("foundLock", async (lock) => {
-  //  console.log(lock.toJSON());
     console.log();
-    
+
     if (lock.isInitialized() && lock.isPaired()) {
       await lock.connect();
-      console.log("Trying to set passage mode");
       console.log();
-      console.log();
+      console.log("Trying to SET a passage mode");
       const result = await lock.setPassageMode({
         type: PassageModeType.WEEKLY,
         weekOrDay: 0,
@@ -27,8 +26,9 @@ async function doStuff() {
         startHour: "0800",
         endHour: "1800"
       });
+      console.log("Result: ", result);
       await lock.disconnect();
-
+      console.log("###");
       process.exit(0);
     }
   });
